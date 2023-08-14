abebabb0b3f552be1490fdd8986e6494d6ff7723
Emiliano <alohacocki@gmail.com>
2023-08-14 19:59:14 -0300

    Implemented TableManager Board

---
+++ Tetris/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    [SerializeField] GameObject _spawnPoint;
+    [SerializeField] GameObject _piece;
+
+
+}
--- Tetris/Assets/Scripts/TableManager.cs
+++ Tetris/Assets/Scripts/TableManager.cs
@@ -5,11 +5,11 @@ using UnityEngine;
 
 public class TableManager : MonoBehaviour
 {
-    [SerializeField] int _row;
-    [SerializeField] int _column;
-    private GameObject[,] _tableDimention => new GameObject[_row,_column];
+    [SerializeField] int _row; //High
+    [SerializeField] int _column; //Width
+    private GameObject[,] _tableDimention => new GameObject[_row, _column];
     [SerializeField] GameObject _pixelPrefab;
-
+    private GameObject _pixel;
 
     private void Awake()
     {
@@ -22,10 +22,25 @@ public class TableManager : MonoBehaviour
         {
             for (int f_column = 0; f_column < _column; f_column++)
             {
-                _tableDimention[f_row, f_column] = Instantiate(_pixelPrefab, transform.position + new Vector3(f_row, f_column, 10), Quaternion.identity);
+                _pixel = Instantiate(_pixelPrefab, transform.position + new Vector3(f_row, f_column, 10), Quaternion.identity);
+                if (f_column == (_column - 1) / 2 && f_row == _row -1)
+                {
+                    Debug.Log("encuentro el medio");
+                    //middle this is the spawn zone
+                    _pixel.GetComponent<SpriteRenderer>().color = Color.gray;
+                }
+                else if (f_column == 0)
+                {
+                    //base of board
+                    _pixel.GetComponent<SpriteRenderer>().color = Color.gray;
+                }
+                else if (f_row == 0 || f_row == _row - 1 || f_column == _column - 1)
+                {
+                    //wall of board
+                    _pixel.GetComponent<SpriteRenderer>().color = Color.gray;
+                }
+                _tableDimention[f_row, f_column] = _pixel;
             }
         }
     }
-
-
 }
