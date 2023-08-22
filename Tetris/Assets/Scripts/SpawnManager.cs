a76a4c0dd04a6e2ac521197fc710e6cb6917da1c
Emiliano <alohacocki@gmail.com>
2023-08-21 21:48:25 -0300

    Implemented SpawnParent position manager

--- Tetris/Assets/Scripts/SpawnManager.cs
+++ Tetris/Assets/Scripts/SpawnManager.cs
@@ -4,8 +4,39 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
-    [SerializeField] GameObject _spawnPoint;
-    [SerializeField] GameObject _piece;
+    private Pixel _spawnPoint;
+    public Piece piece;
+    [SerializeField] private List<Piece> _pieceList;
 
+    private void Start()
+    {
+        PreparedPiece();
+    }
 
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            SpawnPiece();
+        }
+    }
+
+    public void SpawnerPoint(Pixel pixel)
+    {
+        _spawnPoint = pixel;
+    }
+
+    private void SpawnPiece()
+    {
+        Instantiate(piece,_spawnPoint.transform.position,Quaternion.identity);
+    }
+
+    private void PreparedPiece()
+    {
+        for (int i = 0; i < _pieceList.Count; i++)
+        {
+            Instantiate(_pieceList[i],_spawnPoint.transform.position,Quaternion.identity);
+            //_pieceList[i].transform.parent = transform;
+        }
+    }
 }
